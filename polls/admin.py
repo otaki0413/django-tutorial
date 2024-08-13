5c7c3322478d55c6b725730a6d0644c0eb3ddb29
otaki0413 <takida0413@gmail.com>
2024-08-13 18:41:28 +0900

    ✨ adminフォームのカスタマイズ

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Question
 
+
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    # fields = ["pub_date", "question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
