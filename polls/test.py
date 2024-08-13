335d197ac061a09bdb88ec4229d2b36bcad8666f
otaki0413 <takida0413@gmail.com>
2024-08-13 18:54:17 +0900

    🩹 軽微な修正

--- polls/test.py
+++ polls/test.py
@@ -104,7 +104,7 @@ class QuestionDetailViewTests(TestCase):
         returns a 404 not found.
         """
         future_question = create_question(question_text="Future question", days=5)
-        url = reverse("polls:detail", args=(future_question.id))
+        url = reverse("polls:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -114,6 +114,6 @@ class QuestionDetailViewTests(TestCase):
         displays the question's text.
         """
         past_question = create_question(question_text="Past question", days=-5)
-        url = reverse("polls:detail", args=(past_question.id))
+        url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
